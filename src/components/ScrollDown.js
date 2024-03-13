388b85b71b595d7389f2bb33083c426c88459f9a
ninoala <yegor.nino@gmail.com>
2024-03-13 14:05:25 -0400

    created landing section of the home page w/css and scrolldown component

---
+++ src/components/ScrollDown.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../scrolldown.css'
+
+function ScrollDown() {
+  return (
+    <div class="container">
+        <div class="field">
+            <div class="mouse"></div><span>Scroll</span>
+        </div>
+    </div>
+  )
+}
+
+export default ScrollDown
--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHtml5, faCss3Alt, faJs, faReact, faWordpressSimple, faPhp, faShopify } from '@fortawesome/free-brands-svg-icons';
+import Profile from '../assets/profile.jpg';
+import ScrollDown from '../components/ScrollDown';
 
 function PageHome() {
   return (
-    <div id="home">PageHome</div>
+    <div id="home">
+      <section className="landing">
+        <div className="landing-wrapper">
+          <div className="landing-text">
+            <h1>Front-End Web <br /> Developer</h1>
+            <p>Hi, I’m Yegor Nino, your next web dev hero, <br /> 
+    based in Ottawa, ON. </p>
+            <span className='tech-stack'>
+              Tech Stack | 
+              <span className="tech-stack-icons">
+                <FontAwesomeIcon icon={faHtml5} /> 
+                <FontAwesomeIcon icon={faCss3Alt} /> 
+                <FontAwesomeIcon icon={faJs} /> 
+                <FontAwesomeIcon icon={faReact} /> 
+                <FontAwesomeIcon icon={faWordpressSimple} /> 
+                <FontAwesomeIcon icon={faPhp} />
+                <FontAwesomeIcon icon={faShopify} />
+              </span>
+            </span>
+          </div>
+          <figure>
+            <img src={Profile} />
+          </figure>
+        </div>
+        <ScrollDown />
+      </section>
+    </div>
   )
 }
 
